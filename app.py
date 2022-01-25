b8c6db4e8ed50d42e74b55fd4734ed4f1736ca5b
napoles-uach <jnapoles@uach.mx>
2022-01-25 16:15:27 +0000

    Fixes a bug.

--- app.py
+++ app.py
@@ -61,6 +61,19 @@ console.log(foo(5));
 | data   | path to data files to supply the data that will be passed into templates. |
 | engine | engine to be used for processing templates. Handlebars is the default. |
 | ext    | extension to be used for dest files. |
+## Images
+
+![Minion](https://octodex.github.com/images/minion.png)
+![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
+
+Like links, Images also have a footnote style syntax
+
+![Alt text][id]
+
+With a reference later in the document defining the URL location:
+
+[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
+
 '''
 # aqui un comentario hola
 
