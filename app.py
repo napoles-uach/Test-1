01b7d40fe146194a989247de9ba19945d87fee76
napoles-uach <jnapoles@uach.mx>
2022-01-25 16:14:42 +0000

    Fixes a bug.

--- app.py
+++ app.py
@@ -54,6 +54,13 @@ var foo = function (bar) {
 
 console.log(foo(5));
 ```
+## Tables
+
+| Option | Description |
+| ------ | ----------- |
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
 '''
 # aqui un comentario hola
 
