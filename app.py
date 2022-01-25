f1104bd35e4ba6f2dae2e9d1b291f54debbf475e
napoles-uach <jnapoles@uach.mx>
2022-01-25 15:54:58 +0000

    Fixes a bug.

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-import streamlit as st
-st.title('Hola Git')
-st.button('un boton')
-st.slider('Es un slider',0,10)
-st.sidebar.image('streamlit_logo.png')
-st.button('click')
-st.balloons()
 '''
 aqui algo
 
