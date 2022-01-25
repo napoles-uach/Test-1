b83fb359006aec39cc16dab15eefc606f481f1ff
napoles-uach <jnapoles@uach.mx>
2022-01-25 04:16:52 +0000

    Fixes a bug.

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ st.title('Hola Git')
 st.button('un boton')
 st.slider('Es un slider',0,10)
 st.sidebar.image('streamlit_logo.png')
-#st.button('click')
-#st.balloons()
-# aqui un comentario
+st.button('click')
+st.balloons()
+# aqui un comentario h
