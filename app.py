3fab14c85a3211c2659e059896905d35ef7ce959
napoles-uach <jnapoles@uach.mx>
2022-01-25 00:52:19 +0000

    hola

--- app.py
+++ app.py
@@ -2,6 +2,5 @@ import streamlit as st
 st.title('Hola Git')
 st.button('un boton')
 st.slider('Es un slider',0,10)
-#st.image('streamlit_logo.png')
-st.stop()
-st.buton('click')
+st.image('streamlit_logo.png')
+st.button('click')
