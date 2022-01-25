ecd4d9bdd97fb0d996af5e3736cffe32ae31ce11
napoles-uach <jnapoles@uach.mx>
2022-01-25 16:18:06 +0000

    Fixes a bug.

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ see [how to change output](https://github.com/markdown-it/markdown-it-emoji#chan
 ### [Subscript](https://github.com/markdown-it/markdown-it-sub) / [Superscript](https://github.com/markdown-it/markdown-it-sup)
 
 - 19^th^
-- H~2~O
+- H_2_O
 '''
 # aqui un comentario hola
 
